5be6e29a832058c836d344be752efddbb31707b2
Aayush kumar <78674879+sarraayush@users.noreply.github.com>
2022-07-03 23:15:39 +0530

    Add files via upload

---
+++ js_content/script.js
@@ -0,0 +1,38 @@
+/*https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} */
+//1fabce13949e33f44b58f11a83a72de3
+
+let weather = 
+    {
+        apikey: "1fabce13949e33f44b58f11a83a72de3",
+        fetchweather: function(city){
+            fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid="+this.apikey
+                )
+            .then((response) =>response.json())
+            .then((data) => this.displayweather(data));
+            
+            
+        },
+        displayweather: function(data) {
+            
+            const {name} = data;
+            const {icon , description} = data.weather[0];
+            const {temp , humidity} = data.main;
+            const {speed} = data.wind;
+            document.querySelector(".city").innerText = "Weather in " + name;
+            document.querySelector(".icon").src = "";
+            document.querySelector(".description").innerText = description;
+            document.querySelector(".temp").innerText = temp +"°C";
+            document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+            document.querySelector(".wind").innerText= "wind speed: "+ speed + "km\h";
+
+        },
+        search: function (){
+            this.fetchweather(document.querySelector(".search-bar").value)
+        }
+    } 
+
+    document.querySelector("button").addEventListener("click",function(){
+             weather.search();
+    })
+
